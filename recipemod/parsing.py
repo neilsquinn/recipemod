759aabb299baa7f298188b0e7f444478248f6fcf
Neil Quinn <neilq101@gmail.com>
2019-08-11 16:25:44 -0700

    Added basic recipe pulling functionality.

---
+++ recipemod/parsing.py
@@ -0,0 +1,113 @@
+import json
+from datetime import timedelta
+
+import requests
+from bs4 import BeautifulSoup
+
+def parse_iso_8601(iso_duration) -> timedelta:
+    time = iso_duration.split('T')[1]
+    args = {}
+    for char, arg in [('H', 'hours'), ('M', 'minutes'), ('S', 'seconds')]:
+        if time:
+            element = time.split(char)
+            if len(element) > 1:
+                args[arg] = int(element[0])
+                time = element[1]
+    return timedelta(**args)
+
+def extract_ldjson(script_tags):
+    def parse_tree(data, recipes):
+        if type(data) == dict:
+            if data.get('@type') == 'Recipe':
+                recipes += [data]
+            else:
+                graph = data.get('@graph')
+                if graph:
+                    parse_tree(graph, recipes)
+        elif type(data) == list:
+            for item in data:
+                parse_tree(item, recipes)
+    
+    recipes = []
+    for tag in script_tags:
+        parse_tree(json.loads(tag.text), recipes)
+    
+    if not recipes:
+        return None
+    elif len(recipes) > 1:
+        raise ValueError('This page contains more than one recipe:',
+                        [recipe['name'] for recipe in recipes])
+    return recipes[0]
+
+def ldjson_get_image_url(ldjson_recipe):
+    image = ldjson_recipe.get('image')
+    if type(image) == str:
+        return image
+    elif type(image) == list:
+        return image[0]
+    elif type(image) == dict and image['@type'] == 'ImageObject':
+        return image['url']
+    else:
+        return None
+    
+def ldjson_get_instructions(ldjson_recipe):
+    html_to_text = lambda text: BeautifulSoup(text, 'lxml').text.strip()
+    steps = ldjson_recipe.get('recipeInstructions')
+    if steps:
+        if type(steps) == str:
+            return {'type':'one_step', 'steps': [html_to_text(step) for step in steps]}
+        
+        if type(steps) == dict and steps['@type'] == 'ItemList':
+            steps = steps['itemListElement']
+        if type(steps) == list:
+            first_step = steps[0]
+            if type(first_step) == str:
+                return {'type': 'steps', 'steps': [html_to_text(step) for step in steps]}
+            elif type(first_step) == dict:
+                print(first_step['@type'])
+                if 'HowToStep' in first_step['@type']:
+                    return {'type': 'steps', 'steps': [html_to_text(step['text']) for step in steps]}
+                elif 'HowToSection' in first_step['@type']:
+                    sections = []
+                    print('getting sections')
+                    for section in steps:
+                        first_substep = section['itemListElement'][0]
+                        if type(first_substep) == list:
+                            substeps = section['itemListElement']
+                        elif type(first_substep) == dict and 'HowToStep' in first_substep['@type']:
+                            substeps = [step['text'] for step in section['itemListElement']]
+                        sections += [{'name': section['name'],
+                                     'steps': [[html_to_text(step) for step in substeps]]}]
+                    return {'type': 'sections', 'sections': sections}
+    else:    
+        return {'type': None}
+
+def ldjson_get_times(ldjson_recipe):
+    times = {}
+    for key, value in ldjson_recipe.items():
+        if key.endswith('Time'):
+            times[key.replace('Time', '')] = parse_iso_8601(value).seconds
+    return times    
+
+def get_recipe(url, browser_header):
+    recipe = {}
+    r = requests.get(url, headers=browser_header)
+#     breakpoint()
+    if not r:
+        return {'request_error': r.status_code}
+    
+    soup = BeautifulSoup(r.text, 'lxml')
+    ldjson_recipe = extract_ldjson(soup.find_all('script', type='application/ld+json'))
+    if ldjson_recipe:
+        print(ldjson_recipe['recipeIngredient'])
+        recipe.update({key: ldjson_recipe.get(key) 
+                       for key in ['name', 'description', 'url']})
+        recipe['yield'] = ldjson_recipe.get('recipeYield')
+        recipe['image_url'] = ldjson_get_image_url(ldjson_recipe)
+        recipe['instructions'] = ldjson_get_instructions(ldjson_recipe)
+        recipe['times'] = ldjson_get_times(ldjson_recipe)
+        recipe['ingredients'] = ldjson_recipe.get('recipeIngredient')
+        
+        return recipe
+    else:
+        return {'parse_error': 'no recipe JSON found'} # todo look for tags
--- recipemod/recipe.py
+++ recipemod/recipe.py
@@ -5,28 +5,41 @@ from werkzeug.exceptions import abort
 
 from recipemod.auth import login_required
 from recipemod.db import get_db
+from recipemod.parsing import *
+
+from psycopg2.extras import Json
 
 bp = Blueprint('recipe', __name__)
 
-@bp.route('/')
+
+@bp.route('/', methods=('GET', 'POST'))
+@login_required
 def index():
     db = get_db()
     with db.cursor() as c:
+        if request.method == 'POST':
+            url = request.form['url']
+            recipe_data = get_recipe(url, {'User-Agent': request.headers['User-Agent']})
+            recipe_data['user_id'] = g.user['id']
+            for key, value in recipe_data.items():
+                if type(value) in (list, dict):
+                    recipe_data[key] = Json(value)
+#             breakpoint()
+            c.execute(
+                '''INSERT INTO recipes (name, description, yield, ingredients, instructions, times, user_id) 
+                VALUES (%(name)s, %(description)s, %(yield)s, %(ingredients)s, %(instructions)s, %(times)s, %(user_id)s);''',
+                recipe_data)
+            db.commit()
+            return redirect(url_for('recipe.index'))
+        
+        
         c.execute('''SELECT r.id, name, description, created, updated, yield, ingredients, times, category, cuisine, keywords, ratings, video, reviews 
                      FROM recipes r
-                     INNER JOIN users u ON u.id=r.author
+                     INNER JOIN users u ON u.id=r.user_id
                      WHERE u.id=%s;''', (str(g.user['id'])))
         recipes = c.fetchall()
         return render_template('recipe/index.html', recipes=recipes)
 
-@bp.route('/add', methods=('GET', 'POST'))
-@login_required
-def add():
-    if request.method == 'POST':
-        url = request.form('link')
-    db = get_db()
-    with db.cursor() as c:
-        pass
 
 @bp.route('/edit', methods=('GET', 'POST'))
 @login_required
