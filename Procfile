dbb35fbde217005524ef9547220da95f50b7a243
Neil Quinn <neilq101@gmail.com>
2019-08-31 21:46:34 -0700

    Adding Heroku files

---
+++ Procfile
@@ -0,0 +1,1 @@
+gunicorn "recipemod:create_app()" --log-file -
--- recipemod/__init__.py
+++ recipemod/__init__.py
@@ -19,10 +19,6 @@ def create_app(test_config=None):
 	except OSError:
 		pass
 		
-	@app.route('/hello')
-	def hello():
-		return 'Hello world!'
-	
 	from . import db
 	db.init_app(app)
 	
