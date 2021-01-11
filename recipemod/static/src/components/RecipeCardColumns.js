f758e481e57fb2f9b47c302b9cb6d43487397492
Neil Quinn <neilq101@gmail.com>
2021-01-10 21:29:26 -0800

    Starting to switch to bootstrap 5

--- recipemod/static/src/components/RecipeCardColumns.js
+++ recipemod/static/src/components/RecipeCardColumns.js
@@ -5,7 +5,7 @@ function RecipeCardColumns(props) {
   const recipeCards = props.recipes.map((recipe) => 
     <RecipeCard key={recipe.id} recipe={recipe}/>
   );
-  return <div id="recipe-cards" className="card-columns">
+  return <div id="recipe-cards" className="row row-cols-1 row-cols-md-3 g-4">
   {recipeCards}
   </div>
 }
